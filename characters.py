dab98364389f41f8f8aa478ea4fed35a5b851ed0
Robert Morgan <robert.morgan@wisc.edu>
2019-10-26 15:41:11 -0500

    add more characters

--- characters.py
+++ characters.py
@@ -7,8 +7,11 @@ class AsciiCharacter():
     def __init__(self):
         self._characters = [x for x in dir(self) if x.find('__') == -1]
 
-        self._choice = random.choice(self._characters)
+        #make ascii_cat the most probable
+        self._characters += ['ascii_cat'] * (2 * len(self._characters))
 
+        #choose random character
+        self._choice = random.choice(self._characters)
         self.character = eval('self.%s()' %self._choice)
         
     #define new characters as functions below
@@ -31,4 +34,51 @@ class AsciiCharacter():
         return ascii_cat_character
 
 
+    def ascii_seahorse(self):
+        #Ascii Art from https://www.asciiart.eu/animals/
+        #Artist: Morfina
+        ascii_seahorse_character = """
+             / /
+      \\/)/)  /'
+    _'  oo(_.-. 
+  /'.     .---'
+/'-./    (
+)     ; __\\
+\\_.'\ : __|
+     )  _/
+    (  (,.
+     '-.-'
+"""
+        return ascii_seahorse_character
 
+
+    def ascii_bat(self):
+        #https://www.asciiart.eu/animals/bats
+        #Artist: Joan Stark
+        ascii_bat_character = """
+             / / 
+   /\\        /'       /\\
+  / \\'._   (\\_/)   _.'/ \\
+ /_.''._'--('.')--'_.''._\\
+ | \\_ / `;=/ " \\=;` \\ _/ |
+  \\/ `\\__|`\\___/`|__/`  \\/
+   `      \\(/|\\)/       `
+           " ` "
+"""
+        return ascii_bat_character
+
+    def ascii_dog(self):
+        #https://www.asciiart.eu/animals/dogs
+        #Anonymous artist 
+        ascii_dog_character = """
+     |\\_/|   / /               
+     | @ @   /' 
+     |   <>              _  
+     |  _/\\------____ ((| |))
+     |               `--' |   
+ ____|_       ___|   |___.' 
+/_/_____/____/_______|
+"""
+        return ascii_dog_character
+
+    
