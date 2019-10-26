7f1811eceb1255f80ad449ac5da410c8e23bb855
Robert Morgan <robert.morgan@wisc.edu>
2019-10-26 10:31:16 -0500

    update setup for new version

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ setuptools.setup(
 
      name='error_cat',  
 
-     version='0.2',
+     version='0.4',
 
-     scripts=['error_cat', 'error_parsing.py', 'ascii_error.py'] ,
+     scripts=['error_cat', 'error_parsing.py', 'ascii_error.py', 'characters.py'] ,
 
      author="Robert Morgan",
 
@@ -25,6 +25,8 @@ setuptools.setup(
      url="https://github.com/rmorgan10/error_cat",
 
      packages=setuptools.find_packages(),
+
+     python_requires='>=2.6',
 
      classifiers=[
 
